a1d79cbe68cf46db052fd3c5cf4ab6a8072f0a92
Gabriel Bergamini <gabrielbergamini05@gmail.com>
2025-03-11 21:12:59 -0300

    Formatação/format de strings e parâmetros

---
+++ format_string.py
@@ -0,0 +1,13 @@
+nome = "Gabriel"
+idade = 19
+altura = 1.85
+
+linha1 = f'{nome} tem {idade} anos e {altura} de altura'
+#print(linha1)
+
+a = 'A'
+b = 'B'
+c = 1.1
+formato = 'a={nome1} b={nome2} c={nome3:.2f}' .format (nome1=a, nome2=b, nome3=c)
+
+print(formato)
--- variable7.py
+++ variable7.py
@@ -1,7 +1,10 @@
-# Python = Linguagem de programação
-# Tipagem = Dinâmica / Forte
-# str -> string -> texto
-print("Gabriel")
-print("Gabriel\"Bergamini")
-print('Gabriel"Bergamini"')
-print(1, "Gabriel'Bergamini'")
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: Inicie variáveis com letras minúsculas, pode usar números e underlines __
+# O sinal de = é o operador de atribuição. Ele é usado para atribuir um valor a um nome (variável.)
+# Uso: nome_variavel = expressão
+
+nome = "Gabriel"
+idade = 30
+maior_de_idade = idade >= 18
+print("Nome: ", nome, "Idade: ", idade)
+print("É maior? ", maior_de_idade)
