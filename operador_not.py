4bd3a0cf607cd83c0c6951811d0e05bf223a05ed
Gabriel Bergamini <gabrielbergamini05@gmail.com>
2025-03-13 11:15:13 -0300

    Operadores And, Or e Not

---
+++ operador_not.py
@@ -0,0 +1,13 @@
+# Operador lógico "not": usado para inverter expressões
+# not True = False
+# not False = True
+
+senha = input ('Senha: ')
+
+print (type(senha))
+
+if senha == '123456':
+    print('Entrou')
+else:
+    print('Senha incorreta')
+
