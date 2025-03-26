6fcf5852f4f74e527b8e4686f297a1ce38aa31b4
Gabriel Bergamini <gabrielbergamini05@gmail.com>
2025-03-26 11:51:32 -0300

    Aprimorando programa de conta bancária

--- POO/exercicio_conta_bancaria.py
+++ POO/exercicio_conta_bancaria.py
@@ -1,39 +1,74 @@
 
 class ContaBancaria:
-    def __init__(self, nome, saldo, ativo = False):
+    def __init__(self, nome, saldo_conta=0, ativo = False):
         self.nome = nome
-        self.saldo = saldo
+        self.saldo = saldo_conta
         self.ativo = ativo
 
 
     def desativar_conta(self):
-        print(f'Conta desativada!')
+        print(f'Conta de {self.nome} desativada!')
         self.ativo = False
         exit()
 
 
     def ativar_conta(self):
-        if self.ativo:
-            print('Conta ativada!')
+        if not self.ativo:
+            print(f'Conta de {self.nome} ativada!')
             self.ativo = True
+        else:
+            print(f'Conta de {self.nome} ja está ativa!')
 
              
 
-    def depositar(self, saldo):
-        self.saldo = saldo
-        self.ativo = True
-        if self.ativo:
-            saldo = int(input('Escolha o valor do deposito: '))
-            print(f'O valor depositado foi: {saldo}')
+    def depositar(self):
+        if not self.ativo:
+            print("Não é possível depositar. A conta está desativada!")
+            return
+            
+        
     
+        valor = float(input('Escolha o valor do depósito: '))
+        if valor > 0:
+            self.saldo += valor
+            print(f'Depósito de R${valor:.2f} realizado com sucesso!')
+            print(f'Saldo atual: R${self.saldo:.2f}')
+        else:
+            print('O valor do depósito deve ser maior qu zero.')
+
+
+    def sacar(self):
+        if not self.ativo:
+            print('Não é possivel sacar, a conta está desativada!')
+            return
+        
+        valor = float(input('Digite o valor a sacar: '))
+        if valor <= 0:
+            print('O valor deve ser maior que zero')
+        elif valor > self.saldo:
+            print('Saldo insuficiente')
+        else:
+            self.saldo -= valor
+            print(f'Você sacou R${valor:.2f}')
+            print(f'Saldo atual: R${self.saldo:.2f}')
+
+    
+    def exibir_saldo(self):
+        print(f'Saldo de {self.nome}: R${self.saldo:.2f}')
+
+       
+
+           
 
     
     
-conta1 = ContaBancaria('Gabriel', '1500', ativo= True)
-conta2 = ContaBancaria('Bianca', '10000', ativo= False )
-#conta1.desativar_conta()
-conta1.ativar_conta()
-conta1.depositar(0)
+conta1 = ContaBancaria('Gabriel', 1500, ativo= True)
+conta2 = ContaBancaria('Bianca', 10000, ativo= False )
+
+
+conta1.exibir_saldo()
+conta1.depositar()
+conta1.sacar()
 
 
 
