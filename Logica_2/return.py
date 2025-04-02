48e2b68dd9f14c2ed41f89602dea7cb0dbb4d33e
Gabriel Bergamini <gabrielbergamini05@gmail.com>
2025-04-02 10:40:12 -0300

    Revisando return

--- Logica_2/return.py
+++ Logica_2/return.py
@@ -1,7 +1,17 @@
-def saudacao(nome):
-    print(f"Olá, {nome}!")
-    return nome
+# def saudacao(nome):
+#     print(f"Olá, {nome}!")
+#     return nome
 
 
-resultado = saudacao("Carlos")
-  # Saída: Olá, Carlos! \n None
+# resultado = saudacao("Carlos")
+#   # Saída: Olá, Carlos! \n None
+
+def soma(a, b):
+    resultado = a + b
+    return resultado
+
+valor1 = int(input('Digite o primeiro valor: '))
+valor2 = int (input('Digite o segundo valor: '))
+
+resultado = soma(valor1, valor2)
+print (f'O resultado é: {resultado}')
